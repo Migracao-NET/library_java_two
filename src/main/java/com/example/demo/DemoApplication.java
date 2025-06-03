5b5405206d4194b30094ab429b8163a9b6f8785f
sara.f.silva.simao <sara.f.silva.simao@WINAPXHTDDWISEE>
2025-06-02 22:13:28 -0300

    Update package declaration in DemoApplication to match project structure

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.library_java_two;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
